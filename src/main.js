f24f3e3a711b94476b5aaef543c38797dd24e9ea
KittenPixel <79544605+KittenPixel-cell@users.noreply.github.com>
2023-09-20 15:38:19 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -26,5 +26,5 @@ http.createServer((req, res) => {
     res.end('Internal Server Error');
   });
 }).listen(3000, () => {
-  console.log('Server listening on port 8080');
+  console.log('Server listening on port 3000');
 });
