44f18a7dff2a3076f7bfcf6f056a297c67bb7260
KittenPixel <79544605+KittenPixel-cell@users.noreply.github.com>
2023-09-20 15:26:22 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+const https = require('https');
+const tls = require('tls');
+const { Agent } = require('https');
+const url = require('url');
+const http = require('http');
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+class CustomAgent extends Agent {
+  createConnection(options, callback) {
+    options = {
+      ...options,
+      rejectUnauthorized: false, // Ignore SSL certificate errors
+    };
+
+    const socket = tls.connect(options, callback);
+    return socket;
+  }
 }
-bootstrap();
+
+https.Agent = CustomAgent;
+
+http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const targetURL = parsedUrl.query.URL;
+
+  if (!targetURL) {
+    res.writeHead(400, { 'Content-Type': 'text/plain' });
+    res.end('Bad Request: URL parameter missing');
+    return;
+  }
+
+  https.get(targetURL, (response) => {
+    response.on('data', (data) => {
+      res.write(data);
+    });
+    response.on('end', () => {
+      res.end();
+    });
+  }).on('error', (err) => {
+    console.error(err);
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Internal Server Error');
+  });
+}).listen(3000, () => {
+  console.log('Server listening on port 8080');
+});
